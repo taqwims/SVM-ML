48ffa3ad36d0b7366dfc180e8ed8dfdcc57c4e50
taqwims <121000503+taqwims@users.noreply.github.com>
2025-05-17 14:19:17 +0700

    Update pickling.py

--- pickling.py
+++ pickling.py
@@ -1,8 +1,8 @@
 import pickle
 
 # pickling the vectorizer
-pickle.dump(vectorizer, open('models/vectorizer.sav', 'wb'))
+pickle.dump(vectorizer, open('vectorizer.sav', 'wb'))
 # pickling the model
-pickle.dump(classifier_linear, open('models/classifier.sav', 'wb'))
+pickle.dump(classifier_linear, open('classifier.sav', 'wb'))
 
 print('Both vectorizer and classifier has been pickled. Check "classifier_flask" to load and use in flask app')
